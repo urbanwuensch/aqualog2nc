d673165c22daeb2adf2cdab3bec2810a8bd36e82
U. Wünsch <urban.wunsch@chalmers.se>
2026-08-04 06:56:45 +0200

    Update portfile.cmake

--- vcpkg-overlays/libaec/portfile.cmake
+++ vcpkg-overlays/libaec/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Deutsches-Klimarechenzentrum/libaec
     REF v1.1.7
-    SHA512 0
+    SHA512 55bd605590015e0f903a231268265051336c172c18935fdfecead5630454a99811f3f196117556ad1b62f3052d54894fd8e257a9ea9f45cc03f59c93cde00cda
     HEAD_REF main
 )
 
